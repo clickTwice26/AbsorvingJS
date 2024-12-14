b428e90aac341bc39d99756cfa15699f066c1f77
clickTwice26 <shagatoc@gmail.com>
2024-12-14 22:04:19 +0600

    confusion conversion

--- 03_conversionOperation.js
+++ 03_conversionOperation.js
@@ -24,3 +24,44 @@ let someNumber = 33;
 
 someNumber = String(someNumber);
 console.log(typeof someNumber);
+
+console.log("--------------------------")
+// ******** Operations ********
+
+let value = 3;
+let negValue = -value;
+console.log(negValue)
+
+let str1 = "hello";
+let str2 = " hitesh";
+let str3 = str1 + str2;
+console.log(str3);
+
+
+// there are no real life application of this
+// only interview question -> not practically used
+console.log("1"+2); // 12
+console.log(1+"2"); // 12
+console.log("1"+2 + 2); // 12
+console.log(1 + 2 + "2"); // 32
+
+// ECMA ToPrimitive guideline
+    // -> which value type coming first, will be get priority first
+
+
+console.log("Confusion Code: Just to know");
+console.log(+true); // 1
+console.log(1+true); // 2
+console.log(+"");
+console.log("Operator presidence");
+
+console.clear();
+let num1, num2, num3;
+num1 = num2 = num3 = 2+2;
+console.log(num1);
+
+let gameCounter = 100;
+gameCounter++;
+console.log(gameCounter);
+++gameCounter;
+console.log(gameCounter);
