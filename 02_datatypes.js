e6035dc468fb23472b9ac78b9324c87a30511c7d
clickTwice26 <shagatoc@gmail.com>
2024-12-14 12:35:41 +0600

    datatypes

---
+++ 02_datatypes.js
@@ -0,0 +1,26 @@
+"use strict"; // treat all js code as newer version
+console.log("02_variables");
+
+// alert(3+3); // doesnot work in nodejs, works in browser console
+
+// NOTE: code readability should be high
+console.log(3+3); console.log("hitesh");
+
+let name = "hitesh"; //age
+let age = 18; //numerical
+let isLoggedIn = false; //boolean
+
+// number ==> 2^53 maxRange
+// bigint => greater than number;
+// string ==> "" -> double quote
+// boolean ==> true & false
+// null ==> standalone value (empty but not undefined)
+// undefined ==> declared but not assigned
+// symbol ==> unique
+
+// object
+
+console.log(typeof undefined);
+
+
+
