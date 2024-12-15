0d4115cb5ba923c06e9e5fac0ad70dd037768e0d
clickTwice26 <shagatoc@gmail.com>
2024-12-15 23:10:26 +0600

    tricky comparision completed

---
+++ 04_comparison.js
@@ -0,0 +1,18 @@
+console.log(1 < 2);
+console.log(2 >= 1);
+console.log(2 != 2);
+console.clear();
+
+
+console.log("2" > 1); // auto convert "2" into 2
+
+// Ensure the datatype same.
+
+console.log(null > 0);
+console.log("2" === 2); // = = = check value and also datatype
+console.log(2 === 2);
+
+function test(){
+    console.log("test");
+}
+
