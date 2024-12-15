0dbf7768fefafda72f414340ccc69f24c71aaf00
clickTwice26 <shagatoc@gmail.com>
2024-12-16 00:32:48 +0600

    datatype-summary completed

---
+++ datatype-summary.js
@@ -0,0 +1,48 @@
+// primitive / non-primitive datatype (call by reference / call by valye)
+
+// Primitive 7 category
+/*
+(call by value - copied)
+    1. String
+    2. Number
+    3. Boolean
+    4. null
+    5. undefined
+    6. Symbol
+    7. BigInt
+
+*/
+const score = 100;
+const scoreValue = 100.3;
+
+const isLoggedIn = false;
+const outsideTemp = null;
+let userEmail; // undefined; let outsideTemp = undefined;
+
+const id = Symbol('123');
+const anotherId = Symbol('123');
+console.log(id == anotherId);
+
+const bigNumber = 123123123128387123n;
+
+// ** Dynamically typed 
+
+// Reference Data Type
+// Array, Objects, Functions
+
+const heros = ["shaktiman", "naagraj", "doga"];
+let myObject = {
+    name : "hitesh",
+    age : 22,
+    
+} // datatype can be anything.
+
+// Function
+
+const myFunction = function(){
+    console.log("Inside from my function");
+}
+myFunction();
+
+console.log(typeof(myFunction));
+console.log(typeof(outsideTemp));
